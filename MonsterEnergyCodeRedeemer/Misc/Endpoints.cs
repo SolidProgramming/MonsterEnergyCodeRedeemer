0c1abc02b90c5ffc10ebe108f8c9021dc264719d
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-03-21 15:11:16 +0100

    [+] XPath Query Builder

---
+++ MonsterEnergyCodeRedeemer/Misc/Endpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace MonsterEnergyCodeRedeemer.Misc
+{
+    internal static class Endpoints
+    {
+        internal const string Login = "https://www.monsterenergyclawclub.de/login";
+        internal const string CodeRedeem = "https://www.monsterenergyclawclub.de/redeem-code";
+    }
+}
