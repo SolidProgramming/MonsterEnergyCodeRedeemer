dc6595f5e343b10e8928e36b73b1bb34ccca61e7
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2024-03-21 15:00:33 +0100

    Projektdateien hinzufügen.

---
+++ MonsterEnergyCodeRedeemer/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MonsterEnergyCodeRedeemer.Models
+{
+    internal class UserModel
+    {
+        internal string? Email { get; set; }
+        internal string? Password { get; set; }
+        internal string? CSRFToken { get; set; }
+    }
+}
